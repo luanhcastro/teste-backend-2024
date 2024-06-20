4c0a4bacb8bf918359336683465346f85dd629d4
Luan Castro <luanhcvalese@gmail.com>
2024-06-20 19:18:19 -0300

    adding stock to ms-go

--- ms-go/app/models/product.go
+++ ms-go/app/models/product.go
@@ -10,11 +10,12 @@ import (
 type Product struct {
 	ID          int       `json:"id" bson:"id,omitempty"`
 	Name        string    `json:"name" bson:"name,omitempty"`
-	Brand       string    `json:"brand"  bson:"brand,omitempty"`
-	Price       float64   `json:"price"  bson:"price,omitempty"`
-	Description string    `json:"description"  bson:"description,omitempty"`
-	CreatedAt   time.Time `json:"created_at"  bson:"created_at,omitempty"`
-	UpdatedAt   time.Time `json:"updated_at"  bson:"updated_at,omitempty"`
+	Brand       string    `json:"brand" bson:"brand,omitempty"`
+	Price       float64   `json:"price" bson:"price,omitempty"`
+	Description string    `json:"description" bson:"description,omitempty"`
+	Stock       int       `json:"stock" bson:"stock,omitempty"`
+	CreatedAt   time.Time `json:"created_at" bson:"created_at,omitempty"`
+	UpdatedAt   time.Time `json:"updated_at" bson:"updated_at,omitempty"`
 }
 
 func (p *Product) Validate() error {
@@ -29,5 +30,6 @@ func (p *Product) Validate() error {
 		validation.Field(&validate.Price, validation.Required, validation.Min(0.01), validation.Max(1000000.00)),
 		validation.Field(&validate.Brand, validation.Required),
 		validation.Field(&validate.Description, validation.Required),
+		validation.Field(&validate.Stock, validation.Required, validation.Min(0)),
 	)
 }
